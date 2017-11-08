737d1b1663e10ace37d865dc5c9d4c18c549cf58
George Spake <gpspake@gmail.com>
2017-11-07 19:29:17 -0600

    add loading to sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import SignIn from './SignIn';
 import CurrentUser from './CurrentUser';
+import Loading from './Loading';
 
 const App = ({ auth, signIn, signOut }) => {
   return (
@@ -9,6 +10,7 @@ const App = ({ auth, signIn, signOut }) => {
       <div className="Application--sidebar">
         { auth.status === 'ANONYMOUS' && <SignIn signIn={signIn}/> }
         { auth.status === 'SIGNED_IN' && <CurrentUser auth={auth} signOut={signOut} />}
+        { auth.status === 'AWAITING_AUTH_RESPONSE' && <Loading /> }
       </div>
     </main>
   );
