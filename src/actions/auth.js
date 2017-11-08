6f3b59af8fee3670621a1354426f5dfa27b7a650
George Spake <gpspake@gmail.com>
2017-11-07 19:28:36 -0600

    add fake sign in timeout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,15 @@
 export const signIn = () => {
-  return {
-    type: 'SIGN_IN',
-    email: 'bill@example.com',
-    displayName: 'Bill Murray',
-    photoURL: 'http://www.fillmurray.com/200/200',
-    uid: 'firstUser'
+  return dispatch => {
+    dispatch({ type: 'ATTEMPTING_LOGIN' });
+    setTimeout(() => {
+      dispatch({
+        type: 'SIGN_IN',
+        email: 'bill@example.com',
+        displayName: 'Bill Murray',
+        photoURL: 'http://www.fillmurray.com/200/200',
+        uid: 'firstUser'
+      })
+    }, 2000)
   };
 };
 
